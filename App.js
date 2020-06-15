04ffee8efa80ad579df15e76d0594594023c40d0
Sisan Sahu <sisansahu1@gmail.com>
2020-06-15 20:18:39 +0530

    Styling added

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-const calculatebtn= document.querySelector('ion-button')
-const personHeight = document.getElementById('person-height');
-const personWeight = document.getElementById('person-weight');
+const calculatebtn = document.getElementById("calc-btn");
+const resetbtn = document.getElementById("reset-btn");
+const personHeight = document.getElementById("person-height");
+const personWeight = document.getElementById("person-weight");
+const resultArea = document.getElementById('result');
 
-
-
-const  calculateBMI = () =>{
-    height = +personHeight.value;
-    weight = +personWeight.value;
-    const bmi = weight/(weight*height);
-    console.log(bmi);
+const resetBMI = () =>{
+    personHeight.value='';
+    personWeight.value='';
 }
+const calculateBMI = () => {
+  height = +personHeight.value;
+  weight = +personWeight.value;
+  const bmi = weight / (weight * height);
+  console.log(bmi);
 
-calculatebtn.addEventListener('click',calculateBMI)
+  const resultElement = document.createElement('ion-card');
+  resultElement.innerHTML =`
+  <ion-card-content>
+  <h2>${bmi}</h2>
+  </ion-card-content>`
+
+  resultArea.innerHTML='';
+  resultArea.appendChild(resultElement);
+
+  if(isNaN(bmi))
+  {
+      alert('Enter a number');
+      return;
+  }
+};
+
+calculatebtn.addEventListener('click', calculateBMI);
+resetbtn.addEventListener('click',resetBMI);
